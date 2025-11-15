9a3d104a915f221f2a2ba69bf4945f1a4fe5d7a0
EthanDGee <workethangee@gmail.com>
2025-11-15 12:49:17 -0700

    Added reverse look up utility to find closest quote in the source text

--- src/evaluator.py
+++ src/evaluator.py
@@ -1,5 +1,6 @@
 import re
 from src.constants import ATTRIBUTION_TOKEN
+from fuzzysearch import find_near_matches
 
 
 def extract_quotes(generation: str) -> list[str]:
@@ -26,3 +27,31 @@ def extract_quotes(generation: str) -> list[str]:
             quotes.append(quote_text)
 
     return quotes
+
+
+def find_closest_quote(quote: str, source: str, max_dist_ratio: float):
+    max_l_dist = len(quote) * max_dist_ratio
+
+    matches = find_near_matches(quote, source, max_l_dist=max_l_dist)
+
+    best_match = None
+    for match in matches:
+        if best_match is None or match.dist < best_match.dist:
+            best_match = match
+
+    return best_match
+
+
+def _measure_quote_accuracy(quote: str, source: str, max_dist_ratio) -> float:
+    # checks to see if a quote is in the text, if it's not it then attempts to fuzzy find the text until a similar match is found then it measures similarity to the quote
+
+    if quote in source:
+        return 1.0
+
+    best_match = find_near_matches(quote, source, max_l_dist=max_l_dist)
+
+    if not best_match:
+        return 0.0
+
+    min_distance = min(match.dist for match in best_match)
+    return 1.0 - (min_distance / len(quote))
