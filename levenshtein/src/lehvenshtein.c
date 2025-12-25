f0d1dca86b44debaebd834ed93bf4b2ecc8d9868
EthanDGee <workethangee@gmail.com>
2025-12-24 21:25:51 -0700

    Switched to memoization
    Refactored the levenshtein function to use a memoization array for
    improve performance and readability. Added proper base case handling and
    replace recursive calls with iterative logic. Updated test cases and
    output formatting for clarity. Also fixed minor style and naming issues.

--- levenshtein/src/lehvenshtein.c
+++ levenshtein/src/lehvenshtein.c
@@ -1,5 +1,6 @@
 #include <CUnit/Basic.h>
 #include <CUnit/CUnit.h>
+#include <limits.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
@@ -19,6 +20,15 @@ void tailString(char t[], char list[]) {
     t[strlen(list) - 1] = '\0';
 }
 
+int min(int a, int b) {
+    // returns the lowest nonnecative number
+    if (a < b && a >= 0)
+        return a;
+    if (b < 0)
+        return a;
+    return b;
+}
+
 int levenshtein(char *a, char *b) {
     // printf("A:%s\n", a);
     // printf("B:%s\n", b);
@@ -26,39 +36,70 @@ int levenshtein(char *a, char *b) {
     // a per word based implementation of the levenshtein algorithm
     // case where one string is empty
 
-    if (strlen(a) == 0) {
+    int a_length = strlen(a);
+    int b_length = strlen(b);
+
+    if (a_length == 0) {
         printf("A is Empty\n");
-        return strlen(b);
-    } else if (strlen(b) == 0) {
+        return b_length;
+    } else if (b_length == 0) {
         printf("B is Empty\n");
-        return strlen(a);
+        return a_length;
     }
 
-    // get tails of both strings
-    char tailA[strlen(a) - 1];
-    tailString(tailA, a);
-    char tailB[strlen(b) - 1];
-    tailString(tailB, b);
+    // create the memoization array
+    int results[a_length][b_length];
 
-    // if they match recurse on the rest of the string after the first
+    // set base cases
     if (a[0] == b[0])
-        return levenshtein(tailA, tailB);
+        results[0][0] = 0;
+    else
+        // requires deletion and insertion
+        results[0][0] = 2;
 
-    // if they don't match recurse on all combinations and return the mininum
-    int skipBoth = 1 + levenshtein(tailA, tailB);
-    int skipA = 1 + levenshtein(tailA, b);
-    int skipB = 1 + levenshtein(a, tailB);
-    // printf("Distance:%d,%d,%d\n", skipBoth, skipA, skipB);
+    for (int a_i = 0; a_i < a_length; a_i++) {
+        for (int b_i = 0; b_i < b_length; b_i++) {
+            // match case
 
-    if (skipBoth <= skipA && skipBoth <= skipB)
-        return skipBoth;
-    else if (skipA <= skipBoth && skipA <= skipB)
-        return skipBoth;
-    return skipB;
+            int scoreChange;
+            if (a[a_i] == b[b_i])
+                scoreChange = 0;
+            else
+                scoreChange = 1;
+
+            // skip a case
+            int skipA;
+            int skipB;
+            int skipBoth;
+
+            if (a_i != 0)
+                skipA = results[a_i - 1][b_i];
+            else
+                skipA = INT_MIN; // this is done to prevent integer overflow
+
+            // skip b case
+            if (b_i != 0)
+                skipB = results[a_i][b_i - 1];
+            else
+                skipB = INT_MIN;
+
+            // skip both case
+            if (a_i != 0 && b_i != 0)
+                skipBoth = results[a_i - 1][b_i - 1];
+            else
+                skipBoth = INT_MIN;
+
+            // set best to min + scoreChange
+            results[a_i][b_i] = min(skipA, min(skipB, skipBoth)) + scoreChange;
+        }
+    }
+
+    return results[a_length - 1][b_length - 1];
 }
 
 bool testLehvenshtein(char *a, char *b, int expectedResult) {
-    printf("Testing %s-%s\n", a, b) int result = levenshtein(a, b);
+    printf("Testing %s-%s\n", a, b);
+    int result = levenshtein(a, b);
     if (result == expectedResult)
         printf("%s-%s: Passed\n", a, b);
     else
@@ -69,22 +110,22 @@ bool testLehvenshtein(char *a, char *b, int expectedResult) {
 }
 
 int main() {
-    printf("Single Character\n");
+    printf("\nSingle Character\n");
     testLehvenshtein("a", "a", 0);
     testLehvenshtein("b", "a", 2);
     testLehvenshtein("a", "b", 2);
     testLehvenshtein("a", "", 1);
     testLehvenshtein("", "b", 1);
 
-    printf("test_identical_strings\n");
+    printf("\nIdentical Strings\n");
     testLehvenshtein("", "", 0);
     testLehvenshtein("Hello", "Hello", 0);
 
-    printf("Empty Strings\n");
+    printf("\nEmpty Strings\n");
     testLehvenshtein("", "abc", 3);
     testLehvenshtein("abc", "", 3);
 
-    printf("General Cases\n");
+    printf("\nGeneral Cases\n");
     testLehvenshtein("kitten", "sitting", 5);
     testLehvenshtein("flaw", "lawn", 2);
     testLehvenshtein("gumbo", "gambol", 3);
